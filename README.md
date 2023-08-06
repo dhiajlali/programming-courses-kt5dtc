ea120b2142b150eee7541b46c0a9b95296d1abd1
dhiajlali <135439641+dhiajlali@users.noreply.github.com>
2023-08-06 20:25:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# programming-courses-kt5dtc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/programming-courses-kt5dtc)
---
+++ index.ts
@@ -0,0 +1,8 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+let maynumber : number;
+maynumber = parseInt(prompt("enter un numero enter 1 ET 3));
+while(number>=1 && number<=3){number=parseInt(prompt("enter un numero enter 1 ET 3" ))};
+console.log(maynumber);
